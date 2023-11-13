957098ee649f10d1101bacfdd4f7e4375a9d7c88
nicolaeciobota <ciobotanicolae@gmail.com>
2023-11-13 10:16:53 +0000

    ny

--- app/[lng]/awards/[slug]/page.tsx
+++ app/[lng]/awards/[slug]/page.tsx
@@ -2,9 +2,7 @@ import { getFallbackLocale } from "@/app/i18n/settings";
 import queryDatoCMS from "@/utils/queryDatoCMS";
 import { notFound } from "next/navigation";
 import { draftMode } from "next/headers";
-import Post from "@/components/Blog/Post/Post";
 import Award from "@/components/Award/Award/Award";
-import RealTimePost from "@/components/Blog/RealTime/RealTimePost";
 import RealTimeAward from "@/components/Award/RealTime/RealTimeAward";
 import { AwardDocument, SiteLocale } from "@/graphql/generated";
 
