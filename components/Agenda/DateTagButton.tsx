45495d4311f530e7c84379625b73a01dcd3f3df2
nicolaeciobota <ciobotanicolae@gmail.com>
2023-11-19 11:22:20 +0000

    saved

--- components/Agenda/DateTagButton.tsx
+++ components/Agenda/DateTagButton.tsx
@@ -1,9 +1,9 @@
 const DateTagButton = ({
-  eventDate,
+  dateTag,
   lng,
   slug,
 }: {
-  eventDate: string;
+  dateTag: string;
   lng: string;
   slug: string;
 }) => {
@@ -12,7 +12,7 @@ const DateTagButton = ({
       href={`/${lng}/talks/dateTag/${slug}`}
       className="mb-3 mr-3 inline-flex items-center justify-center rounded-md bg-primary bg-opacity-10 px-4 py-2 text-body-color duration-300 hover:bg-opacity-100 hover:text-white"
     >
-      {eventDate}
+      {dateTag}
     </a>
   );
 };
