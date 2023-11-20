69a7a5cf3f031fac46a9dcf16f94d2ab02791585
nicolaeciobota <ciobotanicolae@gmail.com>
2023-11-20 17:18:17 +0000

    stage lik fix

--- components/Agenda/SingleTalk.tsx
+++ components/Agenda/SingleTalk.tsx
@@ -49,7 +49,7 @@ const SingleTalk = ({ talk, locale }: Props) => {
           </div>
         </Link>
         <Link
-          href={"/" + locale + "/" + stage?.slug}
+          href={"/" + locale + "/stage/" + stage?.slug}
           className="relative block h-[230px] w-full overflow-hidden"
         >
           <span className="absolute right-6 top-6 z-20 inline-flex items-center justify-center rounded-full bg-primary px-4 py-2 text-sm font-semibold capitalize text-white">
