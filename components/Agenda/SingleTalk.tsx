0b1673cb7250e9c2a6de713653517e9dcfb3d6ed
nicolaeciobota <ciobotanicolae@gmail.com>
2023-11-21 13:32:14 +0000

    try again

--- components/Agenda/SingleTalk.tsx
+++ components/Agenda/SingleTalk.tsx
@@ -45,40 +45,19 @@ const SingleTalk = ({ talk, locale }: Props) => {
                   <Link href={`/${locale}/talks/speaker/${slug}`}>
                     <DatoImage
                       className="mr-2 h-10 w-10 rounded-full"
-                      layout="fill"
-                      objectFit="cover"
-                      objectPosition="50% 50%"
                       data={picture!.responsiveImage as ResponsiveImage}
                     />
-                    <p className="text-xs font-medium">{name}</p>
-                    <p className="text-xs text-gray-500">{title}</p>
+                    <div>
+                      <p className="text-xs font-medium">{name}</p>
+                      <p className="text-xs text-gray-500">{title}</p>
+                    </div>
                   </Link>
                 </div>
               )
             )}
           </div>
-          <div>
-            {" "}
-            <Link
-              href={"/" + locale + "/stage/" + stage?.slug}
-              className="relative block h-[230px] w-full overflow-hidden"
-            >
-              <span className="absolute right-6 top-6 z-20 inline-flex items-center justify-center rounded-full bg-primary px-4 py-2 text-sm font-semibold capitalize text-white">
-                {stage?.name}
-              </span>
-              <div className="relative h-full w-full overflow-hidden">
-                <DatoImage
-                  className="h-full w-full object-contain"
-                  layout="fill"
-                  objectFit="cover"
-                  objectPosition="50% 50%"
-                  data={seoTags!.image!.responsiveImage as ResponsiveImage}
-                />
-              </div>
-            </Link>
-          </div>
         </div>
-        <p className="text-xs text-gray-500">{start}</p>
+        <p className="text-xs text-gray-500">{dateTags[0].eventDate}</p>
       </div>
     </>
   );
