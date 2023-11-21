f2b704fc24728410a1769a02a08693785a0c76fa
nicolaeciobota <ciobotanicolae@gmail.com>
2023-11-21 13:15:21 +0000

    talk

--- components/Agenda/Talk/Talk.tsx
+++ components/Agenda/Talk/Talk.tsx
@@ -25,56 +25,56 @@ type Props = {
 const Talk = ({ data, lng, speakers }: Props) => {
   if (!data.talk) notFound();
   return (
-    <section className="mt-40 pb-[120px]">
+    <section className="mt-8 pb-8 sm:mt-16 sm:pb-[120px]">
       <div className="container">
         <div className="-mx-4 flex flex-wrap justify-center">
           <div className="w-full px-4 lg:w-8/12">
             <div>
-              <h2 className="mb-8 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight">
+              <h2 className="mb-6 text-2xl font-bold leading-tight text-black dark:text-white sm:text-3xl md:text-4xl lg:text-5xl">
                 {data.talk.title}
               </h2>
-              <p>{data.talk.description}</p>
-              <p>{data.talk.start}</p>
-              <p>{data.talk.end}</p>
-              <div className="mb-10 flex items-center justify-between border-b border-body-color border-opacity-10 pb-4 dark:border-white dark:border-opacity-10">
-                <div className="flex flex-col items-start md:flex-row md:items-center">
-                  <ul className="flex flex-wrap">
-                    {speakers?.map(
-                      ({ id, name, title, slug, picture }: SpeakerRecord) => (
-                        <li key={id} className="m-2 w-48">
-                          <Link href={`/${lng}/talks/speakers/${slug}`}>
-                            <div className="flex flex-col items-center">
-                              <DatoImage
-                                className="h-full w-full object-cover"
-                                layout="fill"
-                                objectFit="cover"
-                                objectPosition="50% 50%"
-                                data={
-                                  picture!.responsiveImage as ResponsiveImage
-                                }
-                              />
-                              <strong className="text-lg">{name}</strong>
-                              <p className="text-sm">{title}</p>
-                              <span className="text-blue-500">
-                                View Speaker Profile
-                              </span>
-                            </div>
-                          </Link>
-                        </li>
-                      )
-                    )}
-                  </ul>
+              <p className="mb-4 text-sm sm:text-base">
+                {data.talk.description}
+              </p>
+              <p className="mb-4 text-sm">{data.talk.start}</p>
+              <p className="mb-6 text-sm">{data.talk.end}</p>
 
-                  {data.talk._publishedAt && (
-                    <div className="mb-5 flex items-center">
-                      <p className="mr-5 flex items-center text-base font-medium text-body-color">
-                        {DateIcon}
-                        {transformDate(data.talk._publishedAt)}
-                      </p>
-                    </div>
+              <div className="mb-8 flex flex-col items-start border-b border-body-color border-opacity-10 dark:border-white dark:border-opacity-10 sm:mb-10 sm:flex-row sm:items-center">
+                <ul className="mb-4 flex flex-wrap sm:mb-0">
+                  {speakers?.map(
+                    ({ id, name, title, slug, picture }: SpeakerRecord) => (
+                      <li key={id} className="m-2 w-48">
+                        <Link href={`/${lng}/talks/speakers/${slug}`}>
+                          <div className="flex flex-col items-center">
+                            <DatoImage
+                              className="h-full w-full rounded-md object-cover"
+                              layout="fill"
+                              objectFit="cover"
+                              objectPosition="50% 50%"
+                              data={picture!.responsiveImage as ResponsiveImage}
+                            />
+                            <strong className="text-lg">{name}</strong>
+                            <p className="text-sm">{title}</p>
+                            <span className="text-blue-500">
+                              View Speaker Profile
+                            </span>
+                          </div>
+                        </Link>
+                      </li>
+                    )
                   )}
-                </div>
-                <div className="mb-5">
+                </ul>
+
+                {data.talk._publishedAt && (
+                  <div className="flex items-center">
+                    <p className="mr-5 flex items-center text-sm font-medium text-body-color">
+                      {DateIcon}
+                      {transformDate(data.talk._publishedAt)}
+                    </p>
+                  </div>
+                )}
+
+                <div className="mt-4 sm:mt-0">
                   <a
                     href={`/${lng}/talks/dates/${data.talk.dateTags[0].slug}`}
                     className="inline-flex items-center justify-center rounded-full bg-primary px-4 py-2 text-sm font-semibold text-white"
@@ -84,25 +84,23 @@ const Talk = ({ data, lng, speakers }: Props) => {
                 </div>
               </div>
 
-              <div className="mt-16 items-center justify-between sm:flex">
-                <div className="mb-5">
+              <div className="mb-8 flex flex-col items-start justify-between sm:mb-16 sm:flex-row sm:items-center">
+                <div className="mb-4 sm:mb-0">
                   <h5 className="mb-3 text-sm font-medium text-body-color">
                     Event Dates:
                   </h5>
-                  <div className="flex items-center">
-                    {data.talk.dateTags.map((eventDate) => {
-                      return (
-                        <DateTagButton
-                          key={eventDate.id}
-                          dateTag={eventDate.eventDate}
-                          lng={lng}
-                          slug={eventDate.slug}
-                        />
-                      );
-                    })}
+                  <div className="flex items-center space-x-2">
+                    {data.talk.dateTags.map((eventDate) => (
+                      <DateTagButton
+                        key={eventDate.id}
+                        dateTag={eventDate.eventDate}
+                        lng={lng}
+                        slug={eventDate.slug}
+                      />
+                    ))}
                   </div>
                 </div>
-                <div className="mb-5">
+                <div>
                   <h5 className="mb-3 text-sm font-medium text-body-color sm:text-right">
                     Share this talk:
                   </h5>
