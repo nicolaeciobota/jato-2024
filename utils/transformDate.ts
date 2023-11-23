f11d256f5cb1c64b71c43cef88af7ee5892608ad
nicolaeciobota <ciobotanicolae@gmail.com>
2023-11-23 10:56:47 +0000

    convert time and date

--- utils/transformDate.ts
+++ utils/transformDate.ts
@@ -5,5 +5,15 @@ export default function transformDate(dateStr: string) {
     date
   );
   const day = date.getDate();
-  return `${month} ${day}, ${year}`;
+
+  // Extract hours and minutes
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  // Format hours and minutes as "hh:mm"
+  const timeFormatted = `${String(hours).padStart(2, "0")}:${String(
+    minutes
+  ).padStart(2, "0")}`;
+
+  return `${month} ${day}, ${year} ${timeFormatted}`;
 }
