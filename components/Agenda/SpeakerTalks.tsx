ec0f3b855333c8fb912fd6ee5b7dd8baea60e7ae
nicolaeciobota <ciobotanicolae@gmail.com>
2023-11-28 10:17:02 +0000

    modi

--- components/Agenda/SpeakerTalks.tsx
+++ components/Agenda/SpeakerTalks.tsx
@@ -12,31 +12,33 @@ type Props = {
 const SpeakerTalks = ({ data, lng }: Props) => {
   if (!data.speaker) notFound();
   return (
-    <section className="px-auto mx-auto mb-8 flex items-center justify-center bg-white px-16 py-[32px] dark:bg-gray-900">
-      <div className="py-10">
-        <div className="flex flex-col items-center justify-center gap-8 p-4 text-center md:flex-row md:text-start">
-          <div className="relative aspect-square w-56 overflow-hidden rounded-xl drop-shadow-xl md:w-72 lg:w-96">
-            <DatoImage
-              className="h-full w-full object-cover"
-              layout="fill"
-              objectFit="cover"
-              objectPosition="top"
-              data={data.speaker.picture!.responsiveImage!}
-            />
-          </div>
-          <div className="xl:mx-4 xl:w-full">
-            <h1 className="text-2xl font-semibold capitalize text-gray-800 dark:text-white lg:text-3xl">
-              {data.speaker.name}
-            </h1>{" "}
-            <h2 className="text-2xl capitalize text-gray-800 dark:text-white lg:text-2xl">
-              {data.speaker.jobTitle}
-            </h2>
-            <div className="mt-4 max-w-2xl text-gray-800 dark:text-gray-300">
-              <ReactMarkdown>{data.speaker.bio}</ReactMarkdown>
+    <section className="mt-16 flex flex-col items-center justify-center py-[32px] ">
+      <section className="px-auto mx-auto mb-8 flex  items-center justify-center bg-white px-2 dark:bg-gray-900">
+        <div className="py-10">
+          <div className="flex flex-col items-center justify-center gap-8  text-center md:flex-row md:text-start">
+            <div className="relative aspect-square w-56 overflow-hidden rounded-xl drop-shadow-xl md:w-72 lg:w-96">
+              <DatoImage
+                className="h-full w-full object-cover"
+                layout="fill"
+                objectFit="cover"
+                objectPosition="top"
+                data={data.speaker.picture!.responsiveImage!}
+              />
+            </div>
+            <div className="xl:mx-4 xl:w-1/2">
+              <h1 className="text-2xl font-semibold capitalize text-gray-800 dark:text-white lg:text-3xl">
+                {data.speaker.name}
+              </h1>{" "}
+              <h2 className="text-2xl capitalize text-gray-800 dark:text-white lg:text-2xl">
+                {data.speaker.jobTitle}
+              </h2>
+              <div className="mt-4 max-w-2xl text-gray-800 dark:text-gray-300">
+                <ReactMarkdown>{data.speaker.bio}</ReactMarkdown>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
 
       <div className="container">
         <div className="-mx-4 flex flex-wrap justify-center">
