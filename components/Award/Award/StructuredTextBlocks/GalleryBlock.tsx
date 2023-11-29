9d8b2d59a4c37585d457bb12958953277208c2b6
nicolaeciobota <ciobotanicolae@gmail.com>
2023-11-29 13:32:37 +0000

    with thumbs

--- components/Award/Award/StructuredTextBlocks/GalleryBlock.tsx
+++ components/Award/Award/StructuredTextBlocks/GalleryBlock.tsx
@@ -39,58 +39,73 @@ const swipePower = (offset: number, velocity: number) => {
 
 // ... (imports remain the same)
 
+// ... (imports remain the same)
+
 const GalleryBlock: React.FC<Props> = ({ galleryRecords }) => {
   const [[page, direction], setPage] = useState([0, 0]);
+  const [activeThumbnail, setActiveThumbnail] = useState(0);
 
   let images = galleryRecords
     .map((record: GalleryRecord, index: number) => {
       return record.imageGallery?.map(
         (image: ImageFileField, imageIndex: number) => (
-          <motion.div
+          <div
             key={`${index}-${imageIndex}`}
             className="relative h-72 w-full overflow-hidden rounded-xl lg:mx-6 lg:h-96 lg:w-1/2"
-            layout
-            initial={{ opacity: 0, x: direction > 0 ? 1000 : -1000 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: direction < 0 ? 1000 : -1000 }}
-            transition={{
-              x: { type: "spring", stiffness: 300, damping: 30 },
-              opacity: { duration: 0.2 },
-            }}
-            drag="x"
-            dragConstraints={{ left: 0, right: 0 }}
-            dragElastic={1}
-            onDragEnd={(e, { offset, velocity }) => {
-              const swipe = swipePower(offset.x, velocity.x);
-
-              if (swipe < -swipeConfidenceThreshold) {
-                paginate(1);
-              } else if (swipe > swipeConfidenceThreshold) {
-                paginate(-1);
-              }
-            }}
           >
-            <DatoImage
-              layout="fill"
-              objectFit="cover"
-              objectPosition="50% 20%"
-              data={image.responsiveImage}
-            />
-            <div className="absolute left-0 top-0 flex h-full w-full items-center justify-between">
-              <div
-                className="prev cursor-pointer pl-2 text-white"
-                onClick={() => paginate(-1)}
-              >
-                {"‣"}
+            <motion.div
+              layout
+              initial={{
+                opacity: 0,
+                y: direction > 0 ? 1000 : -1000,
+                x: direction > 0 ? 1000 : -1000,
+              }}
+              animate={{ opacity: 1, y: 0, x: 0 }}
+              exit={{
+                opacity: 0,
+                y: direction < 0 ? 1000 : -1000,
+                x: direction < 0 ? 1000 : -1000,
+              }}
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                y: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.2 },
+              }}
+              drag="x"
+              dragConstraints={{ left: 0, right: 0 }}
+              dragElastic={1}
+              onDragEnd={(e, { offset, velocity }) => {
+                const swipe = swipePower(offset.x, velocity.x);
+
+                if (swipe < -swipeConfidenceThreshold) {
+                  paginate(1);
+                } else if (swipe > swipeConfidenceThreshold) {
+                  paginate(-1);
+                }
+              }}
+            >
+              <DatoImage
+                layout="fill"
+                objectFit="cover"
+                objectPosition="50% 20%"
+                data={image.responsiveImage}
+              />
+              <div className="absolute left-0 top-0 flex h-full w-full items-center justify-between">
+                <div
+                  className="prev cursor-pointer pl-2 text-white"
+                  onClick={() => paginate(-1)}
+                >
+                  {"‣"}
+                </div>
+                <div
+                  className="next cursor-pointer pr-2 text-white"
+                  onClick={() => paginate(1)}
+                >
+                  {"‣"}
+                </div>
               </div>
-              <div
-                className="next cursor-pointer pr-2 text-white"
-                onClick={() => paginate(1)}
-              >
-                {"‣"}
-              </div>
-            </div>
-          </motion.div>
+            </motion.div>
+          </div>
         )
       );
     })
@@ -100,14 +115,45 @@ const GalleryBlock: React.FC<Props> = ({ galleryRecords }) => {
 
   const paginate = (newDirection: number) => {
     setPage([page + newDirection, newDirection]);
+    setActiveThumbnail(page + newDirection);
   };
 
+  const handleThumbnailClick = (index: number) => {
+    setPage([index, index - page]);
+    setActiveThumbnail(index);
+  };
+
+  const thumbnails = galleryRecords.map(
+    (record: GalleryRecord, index: number) => (
+      <div
+        key={index}
+        className={`thumbnail ${index === activeThumbnail ? "active" : ""}`}
+        onClick={() => handleThumbnailClick(index)}
+      >
+        {/* Add styling and other properties to thumbnails */}
+        <DatoImage
+          className="h-full w-full"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="50% 20%"
+          data={record.imageGallery[0]?.responsiveImage} // Use the first image for thumbnails
+        />
+      </div>
+    )
+  );
+
   return (
-    <>
+    <div className="relative w-full overflow-hidden">
+      <div className="flex">
+        {/* Add other parent container styles here if needed */}
+      </div>
       <AnimatePresence initial={false} custom={direction}>
         {images[imageIndex]}
       </AnimatePresence>
-    </>
+      <div className="thumbnails-container mt-3 flex space-x-2">
+        {thumbnails}
+      </div>
+    </div>
   );
 };
 
