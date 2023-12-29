236b1a7a6ab554523b7daa36ea35f4de2daacf29
MunibQazi12 <munibqazi770@gmail.com>
2023-12-29 11:36:51 +0500

    add SSO with circle

--- app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
- 
-export default function Page() {
-  return <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" unsafeMetadata={{
-    jobRole: 'farig'
-  }}/>;
-}
