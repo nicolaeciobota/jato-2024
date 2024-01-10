0eef2cbb8a4a80db1a8d763bf2e1827880b0eb80
MunibQazi12 <munibqazi770@gmail.com>
2024-01-10 18:34:11 +0500

    add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "node_modules/react-modal-video/css/modal-video.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "@/styles/global.css";
 import { SiteLocale } from "@/graphql/generated";
 import getAvailableLocales from "@/app/i18n/settings";
@@ -27,7 +28,10 @@ export default async function RootLayout({
     <ClerkProvider>
       <html lang={lng}>
         <Head />
-        <body className={`tracking-tight antialiased`}>{children}</body>
+        <body className={`tracking-tight antialiased`}>
+          {children}
+          <SpeedInsights />
+        </body>
       </html>
     </ClerkProvider>
   );
