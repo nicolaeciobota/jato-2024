30cb387835538d36ae1cef96b1b4e06f56e86060
MunibQazi12 <munibqazi770@gmail.com>
2024-01-11 18:27:28 +0500

    test

--- middleware.ts
+++ middleware.ts
@@ -86,49 +86,50 @@ async function getLocale(
 }
 
 export default authMiddleware({
-  beforeAuth: async (request: NextRequest) => {
-    const pathname = request.nextUrl.pathname;
-    const locales = await getAvailableLocales();
-    const pathnameIsMissingLocale = locales.every(
-      (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-    );
+  // beforeAuth: async (request: NextRequest) => {
+  //   const pathname = request.nextUrl.pathname;
+  //   const locales = await getAvailableLocales();
+  //   const pathnameIsMissingLocale = locales.every(
+  //     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  //   );
 
-    if (pathname === '/redirect-to-circle') {
-      return NextResponse.redirect(process.env.NEXT_PUBLIC_CIRCLE_APP || '');
-    }
+  //   if (pathname === '/redirect-to-circle') {
+  //     return NextResponse.redirect(process.env.NEXT_PUBLIC_CIRCLE_APP || '');
+  //   }
 
-    if (pathname === '/') {
-      const locale = await getLocale(request, locales);
-      return NextResponse.redirect(new URL(`/${locale}/home`, request.url));
-    }
-    //go to the specific locale home if there is no pathname but the locale is set
-    if (
-      pathname.split('/').length === 2 &&
-      locales.includes(pathname.split('/')[1] as SiteLocale)
-    ) {
-      return NextResponse.redirect(
-        new URL(`/${pathname.split('/')[1]}/home`, request.url)
-      );
-    }
+  //   if (pathname === '/') {
+  //     const locale = await getLocale(request, locales);
+  //     return NextResponse.redirect(new URL(`/${locale}/home`, request.url));
+  //   }
+  //   //go to the specific locale home if there is no pathname but the locale is set
+  //   if (
+  //     pathname.split('/').length === 2 &&
+  //     locales.includes(pathname.split('/')[1] as SiteLocale)
+  //   ) {
+  //     return NextResponse.redirect(
+  //       new URL(`/${pathname.split('/')[1]}/home`, request.url)
+  //     );
+  //   }
 
-    //go to pathname in browser language if locale is missing but pathname is set
-    if (pathnameIsMissingLocale) {
-      const locale = getLocale(request, locales);
-      return NextResponse.redirect(
-        new URL(`/${locale}/${pathname}`, request.url)
-      );
-    }
-  },
-  afterAuth: async (auth, req: NextRequest, evt) => {
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: '' });
-    }
-  },
+  //   //go to pathname in browser language if locale is missing but pathname is set
+  //   if (pathnameIsMissingLocale) {
+  //     const locale = getLocale(request, locales);
+  //     return NextResponse.redirect(
+  //       new URL(`/${locale}/${pathname}`, request.url)
+  //     );
+  //   }
+  // },
+  // afterAuth: async (auth, req: NextRequest, evt) => {
+  //   if (!auth.userId && !auth.isPublicRoute) {
+  //     return redirectToSignIn({ returnBackUrl: '' });
+  //   }
+  // },
   ignoredRoutes: (req) => {
     const pathname = req.nextUrl.pathname;
     const locale = pathname.split('/')[1];
     return req.url.includes(`/${locale}/home`)
   },
+  publicRoutes: ['/','/en/home']
 })
 
 export const config = {
