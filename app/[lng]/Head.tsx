6f19653e1abf680b247fdade4d8ef6f082f1585a
MunibQazi12 <munibqazi770@gmail.com>
2024-01-18 17:47:06 +0500

    hide preconnect

--- app/[lng]/Head.tsx
+++ app/[lng]/Head.tsx
@@ -5,8 +5,8 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="JATO Annual Meeting" />
       <link rel="icon" href="/images/favicon.ico" />
-      <link rel="preconnect" href="https://accounts.jato-live.com/" />
-      <link rel="preconnect" href="https://social.jato-live.com/" />
+      {/* <link rel="preconnect" href="https://accounts.jato-live.com/" /> */}
+      {/* <link rel="preconnect" href="https://social.jato-live.com/" /> */}
     </head>
   );
 }
