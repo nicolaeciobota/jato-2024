731406a6fffb833396dbb93380eb80f0b7bb7b84
nicolaeciobota <65955997+nicolaeciobota@users.noreply.github.com>
2024-01-30 16:58:59 +0000

    Update HeroEmpty.tsx

--- components/Home/Hero/HeroEmpty.tsx
+++ components/Home/Hero/HeroEmpty.tsx
@@ -17,10 +17,7 @@ const HeroEmpty = ({
           layout="responsive"
           objectPosition="center"
           className='h-auto w-full mt-20'
-          sizes={`100vw, (min-width: 1024px) ${(image?.responsiveImage.width /
-            image?.responsiveImage.height) *
-            100
-            }vh`}
+           data={image?.responsiveImage}
         />
       )}
     </>
