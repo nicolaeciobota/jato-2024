56922d427cade533312e07f068a7f819a476c513
MunibQazi12 <munibqazi770@gmail.com>
2025-01-27 19:11:24 +0500

    add script in head tag

--- app/[lng]/Head.tsx
+++ app/[lng]/Head.tsx
@@ -1,4 +1,9 @@
+"use client";
+import { useAuth } from "@clerk/nextjs";
+
 export default function Head() {
+
+  const { isSignedIn } = useAuth();
   return (
     <head lang='en'>
       <title>JATO Virtual Event Platform</title>
@@ -7,6 +12,35 @@ export default function Head() {
       <link rel="icon" href="/images/favicon.ico" />
       <link rel="preconnect" href="https://accounts.jato-live.com/" />
       <link rel="preconnect" href="https://social.jato-live.com/" />
+      {
+        isSignedIn
+          ? <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function (w, d, s, o, f, js, fjs) {
+              w['circleWidget'] = o;
+              w[o] = w[o] || function () {
+                (w[o].q = w[o].q || []).push(arguments);
+              };
+              js = d.createElement(s);
+              fjs = d.getElementsByTagName(s)[0];
+              js.id = o;
+              js.src = f;
+              js.async = 1;
+              fjs.parentNode.insertBefore(js, fjs);
+            }(window, document, 'script', 'mw', 'https://social.jato-live.com/external/widget.js'));
+  
+            mw('init', {
+              community_public_uid: '02c82c3d',
+              brand_color_dark: '#135352',
+              brand_color_light: '#135352',
+              default_appearance: 'light'
+            });
+          `,
+            }}
+          />
+          : null
+      }
     </head>
   );
 }
